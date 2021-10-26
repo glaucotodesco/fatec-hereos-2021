706049e05cb580015026a1fe4db4354010ee2ed7
Glauco Todesco <glauco.todesco@gmail.com>
2021-10-26 10:26:52 -0300

    Heros Tutorial Create a Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'angular-tour-of-heroes';
+  name  = 'Glauco Todesco';
+  value = 100.20;
+
 }
